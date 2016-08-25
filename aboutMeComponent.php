70632033870a47aee58ae5bc6befb04fdd2c7a60
WaydeTheunissen <wayde.lewis@gmail.com>
2016-08-26 11:01:22 +1200

    Add files via upload

---
+++ aboutMeComponent.php
@@ -0,0 +1,13 @@
+            <section class="container about_me">
+                <article>
+                    <h2><a name="about"></a>about me</h2>                
+                    <p>Outside of web development I have varied hobbies and interests which include home <span>DIY projects</span>, <span>hiking</span>, 
+                        <span>swimming</span>, <span>creative writing</span> and <span>gaming</span>.</p>
+                    <p>I'm also the worlds geekiest <span>science fiction fan</span>, with a neat little collection of models, comics and paraphernalia.<p>
+                    <p>I like having interests that tear me away from my day job. Interests that allow me to be creative in other ways, thereby not stagnating.</p>
+                    <p>As a result it keeps my <span>creativity</span> fresh and often provides a different perspective when doing web development.</p>
+                    
+                    <div class="clearfix margin-bottom-45"></div>
+                    <a href="#top">Top <i class="fa fa-angle-double-up" aria-hidden="true"></i></a>                             
+                </article>
+            </section>
